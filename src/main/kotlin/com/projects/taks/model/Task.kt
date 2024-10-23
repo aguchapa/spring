5218217529ed4568e141fe1cec4e66eef4bc6549
Roberto Agustin Marcialis <41978046+aguchapa@users.noreply.github.com>
2024-10-23 11:03:14 -0300

    feature/api-rest (#1)
    * se agrega la clase TaskController con sus respectivos metodos
    
    * Se indenta el codigo
    
    * se agrega el manejo de excepciones
    
    * se eliminan algunos responseEntity
    
    * se cambio RunTimeException por Exception y PathVariable por RequestParam
    
    ---------
    
    Co-authored-by: Marcela <agustinmarcialis@gmail.com>

---
+++ src/main/kotlin/com/projects/taks/model/Task.kt
@@ -0,0 +1,17 @@
+package com.projects.taks.model
+
+
+ class Task(val id: String?, var title: String, var description: String, var state: State, var category: Category) {
+
+}
+
+enum class State {
+    COMPLETE,
+    INCOMPLETE
+}
+
+enum class Category {
+    ADMIN,
+    DESIGN,
+    FUNCTIONAL,
+}
